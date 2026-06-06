beb97c8a599f65c2dae1a69f6ed62911cad1ddc8
isan127 <i.haruki.yoshida@gmail.com>
2026-06-06 16:11:17 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.STOCK_DASHBOARD_CONFIG = {
   USE_REMOTE_DATA: false,
-  REMOTE_DATA_URL: ""
+  REMOTE_DATA_URL: "https://script.google.com/macros/s/AKfycbzWZsZ3iJrJh1AzBt2ap2E167igvphU9oB3rPQ6US81LzfPbxuBev-6pPZ5abCryHs/exec"
 };
