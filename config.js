b3a858fac42cbb1dbde62a331637f0de14a9b576
isan127 <i.haruki.yoshida@gmail.com>
2026-06-06 16:53:56 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.STOCK_DASHBOARD_CONFIG = {
   USE_REMOTE_DATA: true,
-  REMOTE_DATA_URL: "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnRj4izp-8TrAr6xFT4-Byw11lbfKGhyDufKbnq_HygtpTvpdvWgfLY81iwdmRmDHTRGFix0d6oNtvIVw7v_XvKVUVPWsb-WWYxuWrE01c0qhRZ1b_EX7pKkqABDjbZ-hQpIxkxjo2WAsCpVXSoYWBRFFBn4guSx7q0_dIPCSDJ-fk70Gvuo28hCK23hazkJ2SiH5QoHyDwBW4pD71M_1zir4ceS0opLYsSHa-MKUDeYV-fYsAHg0-Xi5HwgbPZAtbOrUtKv86CVPDbKWeI&lib=MDqwx6VIjmTXhXh61i2rXbujansZJAlqE"
+  REMOTE_DATA_URL: "https://docs.google.com/spreadsheets/d/1uz8J04l8a9E35K9YduA_0UAGbm0QbjZmCegxWGsVfa8/edit?usp=sharing"
 };
